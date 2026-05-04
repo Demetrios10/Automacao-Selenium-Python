963a0fcc95150845be29fc37db7dc7d9a53cc7d3
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-04 18:56:56 -0300

    update

---
+++ Automação-Site-Saucedemo/login_negativo_clicando no botão_sem_dados.py
@@ -0,0 +1,21 @@
+# login no site saucedemo
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Inicializa o driver (ex: Chrome)
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+time.sleep(3)
+
+# clicando no botão login
+botao_login = driver.find_element(By.XPATH,"//input[@type='submit' and @data-test='login-button']")
+botao_login.click()
+time.sleep(5)
+
+
+
+
+
--- curso-selenium/clicking_the_gmail_button.py
+++ curso-selenium/clicking_the_gmail_button.py
@@ -6,3 +6,8 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome()
 browser.get("https://google.com")
 time.sleep(5)
+
+# clicando botão gmail
+gmail = browser.find_element(By.XPATH,"//a[normalize-space()='Gmail']")
+gmail.click()
+time.sleep(5)
