6183dd22fcdb28b294f65f751b0eb6b1701ac7c5
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-06 13:54:48 -0300

    update

---
+++ curso-selenium/comandos_condicionais.py
@@ -0,0 +1,10 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# get() é o comando para acessar um site 
+browser = webdriver.Chrome()
+browser.get("https://")
+browser.maximize_window()
+time.sleep(5)
