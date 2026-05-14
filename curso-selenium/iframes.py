114f7d1c4463c29b7c3e59d39aa657b2915b88ad
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-14 11:57:34 -0300

    up

--- curso-selenium/iframes.py
+++ curso-selenium/iframes.py
@@ -8,12 +8,3 @@ browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get("https://www.gov.br/ds/home")
 time.sleep(10)
-
-browser = browser.find_element(By.XPATH,"/html/body/br-root/main/div/div/br-menu/div/div/div/div/nav/div[1]/a").click()
-time.sleep(5)
-
-
-
-
-
-
