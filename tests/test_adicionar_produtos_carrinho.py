dc1120f8894bcf2ca7604e8c68980e95e0bcd252
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-15 14:16:08 -0300

    update

--- tests/test_adicionar_produtos_carrinho.py
+++ tests/test_adicionar_produtos_carrinho.py
@@ -11,7 +11,3 @@ time.sleep(5)
 driver.find_element(By.ID,"user-name").send_keys("standard_user")
 driver.find_element(By.ID,"password").send_keys("secret_sauce")
 driver.find_element(By.ID,"login-button").click()
-time.sleep(10)
-
-assert driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/span").is_displayed()
-
