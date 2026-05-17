ac2ecfc14bb0880ebb8c32cdf1b9ba56ea948282
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-17 19:00:00 -0300

    update

--- tests/test_adicionar_produtos_carrinho.py
+++ tests/test_adicionar_produtos_carrinho.py
@@ -11,3 +11,9 @@ time.sleep(5)
 driver.find_element(By.ID,"user-name").send_keys("standard_user")
 driver.find_element(By.ID,"password").send_keys("secret_sauce")
 driver.find_element(By.ID,"login-button").click()
+
+driver.find_element(By.XPATH,"/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[1]/a/div").click()
+time.sleep(5)
+
+driver.find_element(By.XPATH, "//button[contains(@class,'btn_inventory') and @data-test='add-to-cart-sauce-labs-backpack']").click()
+time.sleep(5)
