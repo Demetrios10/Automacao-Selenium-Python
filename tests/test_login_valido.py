927a0b72ed8fe67cd4358ecf950644c38d3e8d7e
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-25 11:31:24 -0300

    update

--- tests/test_login_valido.py
+++ tests/test_login_valido.py
@@ -1,21 +1,18 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import conftest
+import pytest
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(5)
-driver.maximize_window()
-driver.get("https://www.saucedemo.com/")
-time.sleep(5)
-
-driver.find_element(By.ID,"user-name").send_keys("standard_user")
-driver.find_element(By.ID,"password").send_keys("secret_sauce")
-driver.find_element(By.ID,"login-button").click()
-time.sleep(10)
-
-assert driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/span").is_displayed()
-
-# salva a evidência
-driver.save_screenshot("evidencias/login_valido.png")
-
-driver.quit()
+@pytest.mark.usefixtures("setup_teardown")
+class TestCT02:
+    def test_login_valido(self):
+        driver = conftest.driver
+        driver.find_element(By.ID,"user-name").send_keys("standard_user")
+        driver.find_element(By.ID,"password").send_keys("secret_sauce")
+        driver.find_element(By.ID,"login-button").click()
+        time.sleep(10)
+        
+        assert driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div[2]/span").is_displayed()
+        
+        # salva a evidência
+        driver.save_screenshot("evidencias/login_valido.png")
