bf67ab95109a1cac8882a22f7c23bfe5aeba5d75
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-27 16:45:50 -0300

    update

--- pages/home_page.py
+++ pages/home_page.py
@@ -4,11 +4,10 @@ from selenium.webdriver.common.by import By
 
 
 class HomePage(BasePage):
-    
+
     def __init__(self):
         self.driver = conftest.driver
         self.titulo_pagina = (By.XPATH,"//span[contains(@class,'title') and @data-test='title' and normalize-space()='Products']")
-    
+
     def verificar_login_com_sucesso(self):
         self.verificar_se_elemento_existe(self.titulo_pagina)
-        
