618eb8bb6e083c52c33e7df71c1d595ca63ca073
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-28 13:51:11 -0300

    update

--- tests/test_ct02_login_valido.py
+++ tests/test_ct02_login_valido.py
@@ -1,6 +1,5 @@
 from pages.home_page import HomePage
 from pages.login_page import LoginPage
-import conftest
 import pytest
 
 @pytest.mark.usefixtures("setup_teardown")
--- tests/test_ct04_realizando_compras_1_produto.py
+++ tests/test_ct04_realizando_compras_1_produto.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from pages.home_page import HomePage
 import pytest
 from pages.login_page import LoginPage
 
@@ -8,7 +9,14 @@ from pages.login_page import LoginPage
 class TestCT04:
     def test_ct04_realizando_compras_1_produto(self):
 
+        # instancia os objetos a serem usados no teste
         login_page = LoginPage()
-        login_page.fazer_login("standard_user", "secret_saucerr")
+        home_page  = HomePage()
+
+        # faz o login
+        login_page.fazer_login("standard_user","secret_sauce")
+
+        #verifica se o login foi realizado
+        home_page.verificar_login_com_sucesso()
 
 
